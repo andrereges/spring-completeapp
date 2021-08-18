195581956804179f432665e81905de9302f0361b
Andrê Reges <dreeh.silva@hotmail.com>
2021-08-18 12:18:53 -0300

    Initial commit

---
+++ src/main/java/br/com/andre/completeapp/AppEndToEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.completeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEndToEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEndToEndApplication.class, args);
+	}
+
+}
