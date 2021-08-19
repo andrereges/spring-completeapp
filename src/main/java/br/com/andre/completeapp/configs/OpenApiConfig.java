0e8de9d4093a98f4665b557da60acb7a690ce697
Andrê Reges <dreeh.silva@hotmail.com>
2021-08-19 13:29:57 -0300

    Implemented documentation with Open Api - clear imports

--- src/main/java/br/com/andre/completeapp/configs/OpenApiConfig.java
+++ src/main/java/br/com/andre/completeapp/configs/OpenApiConfig.java
@@ -1,17 +1,11 @@
 package br.com.andre.completeapp.configs;
 
 import br.com.andre.completeapp.utils.MavenPomPropertyUtil;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.apache.maven.model.Model;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
